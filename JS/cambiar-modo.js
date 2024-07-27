fb5ddc156b188eacf72e53c7daf08e8c923f3230
Anghelo-01 <anghelo_cuba2310@hotmail.com>
2024-07-26 21:55:03 -0500

    Actualizando js

--- JS/cambiar-modo.js
+++ JS/cambiar-modo.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    const desencritarImg = document.querySelector('.desencritar__img__oscuro');
-    const desencritarImg1 = document.querySelector('.desencritar__img__claro');
+
     const cambiarTono = document.getElementById('cambio__modo');
 
     cambiarTono.addEventListener('click', () => {
@@ -12,14 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (modo_oscuro) {
             icon.classList.remove('fa-moon');
             icon.classList.add('fa-sun');
-            desencritarImg1.style.display = 'none';
-            desencritarImg.style.display = 'block';
-            desencritarImg.style.width='50%'
         } else {
             icon.classList.remove('fa-sun');
             icon.classList.add('fa-moon');
-            desencritarImg1.style.display = 'block';
-            desencritarImg.style.display = 'none';
         }
     });
 });
