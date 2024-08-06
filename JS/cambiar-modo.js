91529e7aff20c4900ddc860d42649d0feff8e668
Anghelo-01 <anghelo_cuba2310@hotmail.com>
2024-08-05 19:18:39 -0500

    actualizando cambio de modo

--- JS/cambiar-modo.js
+++ JS/cambiar-modo.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const cambiarTono = document.getElementById('cambio__modo');
 
     cambiarTono.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        const modo_oscuro = document.body.classList.contains('dark-mode');
+        document.body.classList.toggle('modo-oscuro');
+        const modo_oscuro = document.body.classList.contains('modo-oscuro');
         const icon = cambiarTono.querySelector('i');
 
         if (modo_oscuro) {
