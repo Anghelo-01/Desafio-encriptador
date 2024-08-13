b0d1d4be96553d0980d11e171d2a15006c5e08a5
Anghelo-01 <anghelo_cuba2310@hotmail.com>
2024-08-13 13:52:56 -0500

    Agregando elementos a cambiar texto

--- JS/cambiar-idioma.js
+++ JS/cambiar-idioma.js
@@ -1,51 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cambiarIdioma = document.getElementById('cambio__lenguaje');
     
-    let currentLanguage = 'es'; 
+    let lenguajeActual = 'es'; 
 
-    const translations = {
+    const convertir = {
         es: {
-            title: "Encriptador de texto",
-            desencriptarTitle: "Ningún mensaje fue encontrado",
-            desencriptarMessage: "Ingresa el texto que desees encriptar o desencriptar.",
-            footerText: "&copy; copyright 2024 - Encriptador de texto - Desarrollado por Anghelo Cuba",
+            titulo: "Encriptador de texto",
+            tituloDesencriptar: "Ningún mensaje fue encontrado",
+            mensajeEncriptar: "Ingresa el texto aqui.",
+            advertencia: "Solo letras minúsculas y sin acentos.",
+            mensajeDesencriptar: "Ingresa el texto que desees encriptar o desencriptar.",
+            footer: "&copy; copyright 2024 - Encriptador de texto - Desarrollado por Anghelo Cuba",
             encriptarBtn: "Encriptar",
             desencriptarBtn: "Desencriptar",
             copiarBtn: "Copiar",
-            buttonBackground: 'url(img/bandera-es.png)' // Imagen de fondo para el botón en español
+            imagenBoton: 'url(img/bandera-es.png)' // Imagen de fondo para el botón en español
         },
         en: {
-            title: "Text Encryptor",
-            desencriptarTitle: "No message found",
-            desencriptarMessage: "Enter the text you want to encrypt or decrypt.",
-            footerText: "&copy; copyright 2024 - Text Encryptor - Developed by Anghelo Cuba",
+            titulo: "Text Encryptor",
+            tituloDesencriptar: "No message found",
+            mensajeEncriptar: "Enter the text here.",
+            advertencia: "Only lowercase letters and no accents.",
+            mensajeDesencriptar: "Enter the text you want to encrypt or decrypt.",
+            footer: "&copy; copyright 2024 - Text Encryptor - Developed by Anghelo Cuba",
             encriptarBtn: "Encrypt",
             desencriptarBtn: "Decrypt",
             copiarBtn: "Copy",
-            buttonBackground: 'url(img/bandera-eeuu.png)' // Imagen de fondo para el botón en inglés
+            imagenBoton: 'url(img/bandera-eeuu.png)' // Imagen de fondo para el botón en inglés
         }
     };
 
     function cambiarLenguaje(language) {
-        const t = translations[language];
+        const cambiarTexto = convertir[language];
         
-        document.querySelector('h1').textContent = t.title;
-        document.getElementById('desencriptar__mensaje').setAttribute('placeholder', t.desencriptarMessage);
-        document.querySelector('.desencritar h2').textContent = t.desencriptarTitle;
-        document.querySelector('.desencritar p').textContent = t.desencriptarMessage;
-        document.querySelector('.footer p').innerHTML = t.footerText;
-        document.getElementById('Encriptar').textContent = t.encriptarBtn;
-        document.getElementById('Desencriptar').textContent = t.desencriptarBtn;
-        document.getElementById('desencriptar__boton').textContent = t.copiarBtn;
+        document.querySelector('h1').textContent = cambiarTexto.titulo;
+        document.getElementById('inputTexto').setAttribute('placeholder', cambiarTexto.mensajeEncriptar);
+        document.getElementById('texto_advertencia').textContent = cambiarTexto.advertencia;
+
+        document.getElementById('desencriptar__mensaje').setAttribute('placeholder', cambiarTexto.mensajeDesencriptar);
+        document.querySelector('.desencritar h2').textContent = cambiarTexto.tituloDesencriptar;
+        document.querySelector('.desencritar p').textContent = cambiarTexto.mensajeDesencriptar;
+        document.querySelector('.footer p').innerHTML = cambiarTexto.footer;
+        document.getElementById('Encriptar').textContent = cambiarTexto.encriptarBtn;
+        document.getElementById('Desencriptar').textContent = cambiarTexto.desencriptarBtn;
+        document.getElementById('desencriptar__boton').textContent = cambiarTexto.copiarBtn;
 
         // Cambiar el fondo del botón de idioma
         cambiarIdioma.style.backgroundImage = t.buttonBackground;
     }
 
     cambiarIdioma.addEventListener('click', () => {
-        currentLanguage = currentLanguage === 'es' ? 'en' : 'es';
-        cambiarIdioma.textContent = currentLanguage.toUpperCase();
-        cambiarLenguaje(currentLanguage);
+        lenguajeActual = lenguajeActual === 'es' ? 'en' : 'es';
+        cambiarIdioma.textContent = lenguajeActual.toUpperCase();
+        cambiarLenguaje(lenguajeActual);
     });
 
     // Inicializa el idioma
